4de288352985c14aad84724bf784d38b0804f9c3
Morgan Fouesneau <morgan@fenrir>
2013-01-12 11:19:14 -0800

    Updated doc path

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ __version__ = '1.0'
 __author__  = 'MF'
 import os, inspect
 localpath = '/'.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split('/')[:-1])
-__doc__ = open(localpath+'/README.rst').read()
+__doc__ = open(localpath+'/README.md').read()
 
