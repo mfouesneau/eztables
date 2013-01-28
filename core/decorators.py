2f7d570f60e7aa78f589b330aaea39fea4787b00
Morgan Fouesneau <morgan@fenrir>
2013-01-28 14:19:55 -0800

    Added warning and deprecated decorators

--- core/decorators.py
+++ core/decorators.py
@@ -1,131 +1,163 @@
 """ Some useful decorators """
 
-import time, math, sys
+import time
+import math
 import functools
 from functools import wraps
 from multiprocessing import Pool
-import subprocess
+import warnings
+
 
 class timeit(object):
-	""" Time a block of your code.
-	This can be used as a 
+    """ Time a block of your code.
+    This can be used as a
 
-	CONTEXT MANAGER:
+    CONTEXT MANAGER:
 
-		with timeit(text):
-			<code>
-	
-		text	str	Text to display	
+        with timeit(text):
+            <code>
 
-	as a FUNCTION DECORATOR
-		@timeit(verbose=True)
-		def myfunc(...):
-		   ...
+        text    str Text to display
 
-		you can then call back the time values associated to your
-		function: myfunc.time
-	
-	KEYWORDS:
-		f	function
-	"""
-	def __init__(self, f=None, verbose=True, text=None):
-		self.f = f
-		if not self.f is None:
-			if type(self.f) != str:
-				functools.update_wrapper(self, f)
-				self.text = self.__name__
-			else:
-				self.text = f 
+    as a FUNCTION DECORATOR
+        @timeit(verbose=True)
+        def myfunc(...):
+           ...
 
-		else:
-			self.text = text or ''
-		self.verbose = verbose
+        you can then call back the time values associated to your
+        function: myfunc.time
 
-	def __enter__(self):
-		print "Timing %s" % (self.text)
-		self.start = time.time()
+    KEYWORDS:
+        f   function
+    """
+    def __init__(self, f=None, verbose=True, text=None):
+        self.f = f
+        if not self.f is None:
+            if type(self.f) != str:
+                functools.update_wrapper(self, f)
+                self.text = self.__name__
+            else:
+                self.text = f
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.stop = time.time()
-		print self.time
+        else:
+            self.text = text or ''
+        self.verbose = verbose
 
-	def __pretty_print(self, t):
-		units = [u"s", u"ms",u'us',"ns"]
-		scaling = [1, 1e3, 1e6, 1e9]
-		if t > 0.0 and t < 1000.0:
-			order = min(-int(math.floor(math.log10(t)) // 3), 3)
-		elif best >= 1000.0:
-			order = 0
-		else:
-			order = 3
+    def __enter__(self):
+        print "Timing %s" % (self.text)
+        self.start = time.time()
 
-		return "%s Execution time: %.3g %s" % (self.text, t * scaling[order], units[order])
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop = time.time()
+        print self.time
 
-	@property	
-	def time(self):
-		return self.__pretty_print(self.stop-self.start)
+    def __pretty_print(self, t):
+        units = [u"s", u"ms", u'us', "ns"]
+        scaling = [1, 1e3, 1e6, 1e9]
+        if t > 0.0 and t < 1000.0:
+            order = min(-int(math.floor(math.log10(t)) // 3), 3)
+        elif t >= 1000.0:
+            order = 0
+        else:
+            order = 3
 
-	def __call__(self, *args, **kwargs):
-		self.start = time.time()
-		r = self.f(*args, **kwargs)
-		self.stop = time.time()
-		if self.verbose:
-			print self.time
-		return r
+        return "%s Execution time: %.3g %s" % (self.text, t * scaling[order], units[order])
+
+    @property
+    def time(self):
+        return self.__pretty_print(self.stop - self.start)
+
+    def __call__(self, *args, **kwargs):
+        self.start = time.time()
+        r = self.f(*args, **kwargs)
+        self.stop = time.time()
+        if self.verbose:
+            print self.time
+        return r
+
 
 class memoize(dict):
-	'''Decorator. Caches a function's return value each time it is called.
-	If called later with the same arguments, the cached value is returned
-	(not reevaluated).
-	Fastest implementation according to http://code.activestate.com/recipes/578231/ 
-	'''
-	def __init__(self, func):
-		self.func = func
-		functools.update_wrapper(self, func)
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    Fastest implementation according to http://code.activestate.com/recipes/578231/
+    '''
+    def __init__(self, func):
+        self.func = func
+        functools.update_wrapper(self, func)
 
-	def __getitem__(self, *key):
-		return dict.__getitem__(self, key)
+    def __getitem__(self, *key):
+        return dict.__getitem__(self, key)
 
-	def __missing__(self, key):
-		ret = self[key] = self.func(*key)
-		return ret
+    def __missing__(self, key):
+        ret = self[key] = self.func(*key)
+        return ret
 
-	__call__ = __getitem__
+    __call__ = __getitem__
 
-	def __repr__(self):
-		'''Return the function's docstring.'''
-		return self.func.__doc__
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+
 
 def run_async(func, pool=None):
-	"""
-	run_async(func)
-		function decorator, intended to make "func" run in a separate
-		thread (asynchronously).
-		Returns the created Thread object
+    """
+    run_async(func)
+        function decorator, intended to make "func" run in a separate
+        thread (asynchronously).
+        Returns the created Thread object
 
-		E.g.:
-		@run_async
-		def task1():
-			do_something
+        E.g.:
+        @run_async
+        def task1():
+            do_something
 
-		@run_async
-		def task2():
-			do_something_too
+        @run_async
+        def task2():
+            do_something_too
 
-		t1 = task1()
-		t2 = task2()
-		...
-		t1.join()
-		t2.join()
-	"""
-	_pool = pool or Pool()
+        t1 = task1()
+        t2 = task2()
+        ...
+        t1.join()
+        t2.join()
+    """
+    _pool = pool or Pool()
 
-	@wraps(func)
-	def async_func(*args, **kwargs):
-		func_hl = _pool.Process(target = func, args = args, kwargs = kwargs)
-		func_hl.start()
-		return func_hl
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        func_hl = _pool.Process(target=func, args=args, kwargs=kwargs)
+        func_hl.start()
+        return func_hl
 
-	return async_func
+    return async_func
 
 
+def deprecated(message=None):
+    """
+    This is a decorator which can be used to mark functions as deprecated. It
+    will result in a warning being emitted when the function is used.
+    """
+    def deco(func):
+        @wraps(func)
+        def new_func(*args, **kwargs):
+            warnings.warn("Call to deprecated function {}. {}".format(func.__name__, message or ''),
+                        category=DeprecationWarning)
+            return func(*args, **kwargs)
+        return new_func
+    return deco
+
+
+def warning(message=None, category=UserWarning):
+    """
+    This is a decorator which can be used to mark functions with warnings. It
+    will result in a warning being emitted when the function is used.
+    """
+    def deco(func):
+        @wraps(func)
+        def new_func(*args, **kwargs):
+            warnings.warn("Call to function {}. {}".format(func.__name__, message or ''),
+                        category=category)
+            return func(*args, **kwargs)
+        return new_func
+    return deco
